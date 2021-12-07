d7b025c57c47a1989261ff52c43fbe04b3c96dcc
tertere <tertere@>
2021-12-07 21:30:00 +0300

    Complete!

---
+++ 3. Highest Prime Factor Calculator.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int num;
+    int del = 2;
+    int fac = 2;
+    while (true) {
+        cout << "Enter the number: ";
+        cin >> num;
+        while (num != 1) {
+            while (num % del == 0) {
+                num /= del;
+                fac = del;
+            }
+            del++;
+        }
+        del = 2;
+        cout << fac << "\n";
+    }
+}
